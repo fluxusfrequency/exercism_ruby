d6016840997966760335b89dc5a824807a441b1b
Ben Lewis <bennlewis@gmail.com>
2014-11-03 06:05:42 -0700

    update etl

--- etl/etl.rb
+++ etl/etl.rb
@@ -1,8 +1,8 @@
 module ETL
   def self.transform(scores_by_rack)
-    scores_by_rack.each_with_object({}) do |(score, rack), scores_by_tile|
-      rack.each do |letter|
-        scores_by_tile[letter.downcase] = score
+    scores_by_rack.each_with_object({}) do |(score, tiles), scores_by_tile|
+      tiles.each do |tile|
+        scores_by_tile[tile.downcase] = score
       end
     end
   end
